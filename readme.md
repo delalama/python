cf589726ca3ea28b18c3fccbee5bc6ff0779cf28
JesusDLL <elaltas@gmail.com>
2023-12-06 17:54:51 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+
+
+
+IA BOOKS
+https://github.com/fadcrep/the-best-artificial-intelligence-books?search=1
+
+LIBRARIES
+Numpy
+pip3 install numpy
---
+++ src/exercises/test/operations.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(1, 1)  # add assertion here
+
+
